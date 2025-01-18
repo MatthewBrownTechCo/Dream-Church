e03335878b3e0306fab2e7b93e457518c954efa6
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2025-01-17 22:38:18 -0500

    Finished home page skeleton along with Footer component

---
+++ dc-application/src/app/components/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="ml-40 mt-[100px]">
+      <div>
+        <h1 className="text-2xl font-bold">Contact Info</h1>
+        <p>hello@dreamcolumbia.com</p>
+        <p>1633 Main St</p>
+        <p>Suite 100</p>
+        <p>Columbia, SC 29201</p>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">Follow Us</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- dc-application/src/app/home/page.tsx
+++ dc-application/src/app/home/page.tsx
@@ -2,10 +2,13 @@
 
 import Logo from "../components/logos";
 import { useNavigate } from "../components/buttons";
+import Footer from "../components/footer";
 
 export default function Home() {
   const inPersonNavigate = useNavigate("/inperson.tsx");
   const onlineNavigate = useNavigate("/online.tsx");
+  const aboutNavigate = useNavigate("/about.tsx");
+  const bioNavigate = useNavigate("/bio.tsx");
 
   return (
     <>
@@ -60,6 +63,139 @@ export default function Home() {
           ways of his Kingdom
         </h2>
       </section>
+      <div className="flex justify-center items-center flex-col mt-[20px] gap-4">
+        <button
+          className="bg-transparent border border-black py-[20px] w-[500px] hover:bg-gray-100"
+          onClick={aboutNavigate}
+        >
+          ABOUT US
+        </button>
+        <button
+          className="bg-transparent border border-black py-[20px] w-[500px] hover:bg-gray-100"
+          onClick={bioNavigate}
+        >
+          ABOUT JOSHUA BROWN
+        </button>
+      </div>
+      <section className="text-center mt-[100px]">
+        <h2 className="text-2xl font-bold">Follow us on social media</h2>
+        <p>(Social Media buttons will go here)</p>
+      </section>
+      <form className="mt-[100px] ml-[30%] space-y-6">
+        <h2 className="text-2xl font-bold">Contact Us</h2>
+        <h3 className="font-bold">
+          We'd love to hear from you. Fill out the form below to get started.
+        </h3>
+
+        <div className="space-y-2">
+          <label htmlFor="f-name" className="block font-medium">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="fname"
+            placeholder="First Name"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="l-name" className="block font-medium">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lname"
+            placeholder="Last Name"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="email" className="block font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="example@email.com"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="phone" className="block font-medium">
+            Phone
+          </label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="XXX-XXX-XXXX"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          />
+        </div>
+
+        <div className="flex space-x-[10%]">
+          <div>
+            <label htmlFor="phone" className="block font-medium mr-2">
+              Call
+            </label>
+            <input
+              type="checkbox"
+              className="form-checkbox h-5 w-5 bg-transparent checked:bg-black checked:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="phone" className="block font-medium mr-2">
+              Text
+            </label>
+            <input
+              type="checkbox"
+              className="form-checkbox h-5 w-5 bg-transparent checked:bg-black checked:text-white"
+            />
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="email" className="block font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="example@email.com"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="message" className="block font-medium">
+            Message
+          </label>
+          <textarea
+            rows={5}
+            name="message"
+            placeholder="Enter your message here"
+            className="border border-black w-[55%] p-2"
+            required={true}
+          ></textarea>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="bg-transparent border border-black py-[15px] w-[15%] hover:bg-gray-100"
+          >
+            SUBMIT
+          </button>
+        </div>
+      </form>
+      <Footer />
     </>
   );
 }
