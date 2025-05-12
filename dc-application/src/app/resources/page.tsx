4587feee8da988eae337f32d37a4f9323094cec3
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2025-05-12 14:24:24 -0400

    added week 2 resources for Into the Silent Land series

--- dc-application/src/app/resources/page.tsx
+++ dc-application/src/app/resources/page.tsx
@@ -55,17 +55,57 @@ export default function Resources() {
             Chuck Degroat
           </a>
         </p>
+      </div>
+
+      <div className="text-center pt-10 pb-20 space-y-3 bg-white text-black px-8 md:px-0">
+        <h2 className="text-3xl md:text-3xl">
+          <b>Week 2: Prayer and Scripture</b>
+        </h2>
+        <br />
         <p>
-          <b>Rule of Life Workbook: </b>
+          <b>Biblical Text: </b>
           <a
-            href="https://static1.squarespace.com/static/5852ec829f7456cfcc0e1c5a/t/5e17be5d7cba366ace1df6e1/1578614383140/ptw_ruleoflife_workbook_.pdf"
+            href="https://www.bible.com/bible/2016/PSA.27.NRSV"
             target="_blank"
             className="hover:text-gray-700"
           >
-            From “Practicing the Way”
+            Psalm 27:1-5 NRSV
+          </a>
+        </p>
+        <p>
+          <b>Book: </b>
+          <a
+            href="https://www.amazon.com/Answering-God-Psalms-Tools-Prayer/dp/0060665122/ref=sr_1_1?crid=351R894EL4W43&dib=eyJ2IjoiMSJ9.m8zgZnpDR1JHU6HgusG20NTuAvoakuNMagFgC-b4DRXQrudIYpOkdfWyrWTOsm0qJYalju-rX_Y4TzoElFly9-AKbwAV_iT7E44uhxsRL3Yzy_saBdXQyu0haLzB4mOEne386OmWXyQWzRnZlShkQBoMpiN_ijlrexuNrKxIqE2Sa2nXTw3AdjDWxKte-50LU5bniBuOneNeCJf7ijgdGImuF9Z6NY-omhbCTAQUG2c.UopjYwCoVWz2OqWOPYEXjZkzhr6Xk7sx3TqwhMKMFBE&dib_tag=se&keywords=eugene+peterson+prayer&qid=1746995330&sprefix=eugene+peterson+prayer%2Caps%2C163&sr=8-1"
+            target="_blank"
+            className="hover:text-gray-700"
+          >
+            The Psalms as Tools for Prayer by Eugene Peterson
+          </a>
+        </p>
+        <p>
+          <b>
+            Article on <i>Lectio Divina</i>:{" "}
+          </b>
+          <a
+            href="/files/overview_LectioDivina.pdf"
+            target="_blank"
+            className="hover:text-gray-700"
+          >
+            "Overview of Lectio Divina" by J.I. Packer and Carolyn Nystrom
           </a>
         </p>
       </div>
+
+      {/* <p>
+        <b>Rule of Life Workbook: </b>
+        <a
+          href="https://static1.squarespace.com/static/5852ec829f7456cfcc0e1c5a/t/5e17be5d7cba366ace1df6e1/1578614383140/ptw_ruleoflife_workbook_.pdf"
+          target="_blank"
+          className="hover:text-gray-700"
+        >
+          From “Practicing the Way”
+        </a>
+      </p> */}
       <Footer />
     </>
   );
