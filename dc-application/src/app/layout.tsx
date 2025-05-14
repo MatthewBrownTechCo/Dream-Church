fb03bbc2a6efa52c54dbf0e40fbd064b85a79fa4
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2025-05-13 21:35:07 -0400

    Added SpeedInsights to project

--- dc-application/src/app/layout.tsx
+++ dc-application/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Cormorant_Garamond } from "next/font/google";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
