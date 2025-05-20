b4d40bea423a5b7a2bc3130d99a0be30140b244d
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2025-05-20 16:33:27 -0400

    Added proper 404 page, changed the order of the resource page and underlined links there

--- dc-application/src/app/not-found.tsx
+++ dc-application/src/app/not-found.tsx
@@ -1,5 +1,27 @@
-import { redirect } from "next/navigation";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function NotFound() {
-  redirect("/");
+  return (
+    <>
+      <NavBar />
+
+      <main className="relative bg-[url('/images/matt-drums.jpg')] bg-cover bg-center text-white flex flex-col justify-center text-center py-40">
+        <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
+        <h1 className="relative text-[100px] md:text-[200px] font-bold py-10 md:py-20">
+          404
+        </h1>
+        <h2 className="relative text-[50px] md:text-[100px] py-5 md:py-10">
+          Not Found
+        </h2>
+        <a href="/" rel="noopener noreferrer">
+          <button className="relative text-black bg-white border border-white p-[15px] w-[50%] md:w-[180px] hover:bg-gray-200 text-2xl font-bold mt-8">
+            Home
+          </button>
+        </a>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
--- dc-application/src/app/resources/page.tsx
+++ dc-application/src/app/resources/page.tsx
@@ -17,45 +17,21 @@ export default function Resources() {
           </p>
         </header>
       </div>
+
       <div className="text-center py-20 space-y-3 bg-white text-black px-8 md:px-0">
         <h2 className="text-3xl md:text-3xl">
-          <b>Week 1: You Are Blessed</b>
+          <b>Week 3: Rule of Life</b>
         </h2>
-        <br />
+
         <p>
-          <b>Biblical Text: </b>
+          <b>Rule of Life Workbook: </b>
           <a
-            href="https://www.bible.com/bible/97/MAT.5.NIV?parallel=2016"
+            href="https://static1.squarespace.com/static/5852ec829f7456cfcc0e1c5a/t/5e17be5d7cba366ace1df6e1/1578614383140/ptw_ruleoflife_workbook_.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-700"
+            className="hover:text-gray-700 underline underline-offset-2"
           >
-            Matthew 5:1-12 MSG, NRSV
-          </a>
-        </p>
-        <p>
-          <b>Book: </b>
-          <a
-            href="https://www.amazon.com/dp/0195307607?ref=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&ref_=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&social_share=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&bestFormat=true&previewDoh=1"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-700"
-          >
-            Into the Silent Land by Martin Laird
-          </a>
-        </p>
-        <p>
-          <b>
-            Article on a <i>Rule of Life</i>:{" "}
-          </b>
-          <a
-            href="https://www.chuckdegroat.net/chuck-degroat-blog/longings-over-legalism-in-your-new-years-aspirations"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-700"
-          >
-            &quot;Longings Over Legalism in Your New Year&apos;s
-            Aspirations&quot; by Chuck Degroat
+            From “Practicing the Way”
           </a>
         </p>
       </div>
@@ -71,7 +47,7 @@ export default function Resources() {
             href="https://www.bible.com/bible/2016/PSA.27.NRSV"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-700"
+            className="hover:text-gray-700 underline underline-offset-2"
           >
             Psalm 27:1-5 NRSV
           </a>
@@ -82,7 +58,7 @@ export default function Resources() {
             href="https://www.amazon.com/Answering-God-Psalms-Tools-Prayer/dp/0060665122/ref=sr_1_1?crid=351R894EL4W43&dib=eyJ2IjoiMSJ9.m8zgZnpDR1JHU6HgusG20NTuAvoakuNMagFgC-b4DRXQrudIYpOkdfWyrWTOsm0qJYalju-rX_Y4TzoElFly9-AKbwAV_iT7E44uhxsRL3Yzy_saBdXQyu0haLzB4mOEne386OmWXyQWzRnZlShkQBoMpiN_ijlrexuNrKxIqE2Sa2nXTw3AdjDWxKte-50LU5bniBuOneNeCJf7ijgdGImuF9Z6NY-omhbCTAQUG2c.UopjYwCoVWz2OqWOPYEXjZkzhr6Xk7sx3TqwhMKMFBE&dib_tag=se&keywords=eugene+peterson+prayer&qid=1746995330&sprefix=eugene+peterson+prayer%2Caps%2C163&sr=8-1"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-700"
+            className="hover:text-gray-700 underline underline-offset-2"
           >
             The Psalms as Tools for Prayer by Eugene Peterson
           </a>
@@ -95,7 +71,7 @@ export default function Resources() {
             href="/files/overview_LectioDivina.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-700"
+            className="hover:text-gray-700 underline underline-offset-2"
           >
             &quot;Overview of Lectio Divina&quot; by J.I. Packer and Carolyn
             Nystrom
@@ -105,18 +81,43 @@ export default function Resources() {
 
       <div className="text-center py-20 space-y-3 bg-white text-black px-8 md:px-0">
         <h2 className="text-3xl md:text-3xl">
-          <b>Week 3: Rule of Life</b>
+          <b>Week 1: You Are Blessed</b>
         </h2>
-
+        <br />
         <p>
-          <b>Rule of Life Workbook: </b>
+          <b>Biblical Text: </b>
           <a
-            href="https://static1.squarespace.com/static/5852ec829f7456cfcc0e1c5a/t/5e17be5d7cba366ace1df6e1/1578614383140/ptw_ruleoflife_workbook_.pdf"
+            href="https://www.bible.com/bible/97/MAT.5.NIV?parallel=2016"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-700"
+            className="hover:text-gray-700 underline underline-offset-2"
           >
-            From “Practicing the Way”
+            Matthew 5:1-12 MSG, NRSV
+          </a>
+        </p>
+        <p>
+          <b>Book: </b>
+          <a
+            href="https://www.amazon.com/dp/0195307607?ref=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&ref_=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&social_share=cm_sw_r_ffobk_cp_ud_dp_N1A29YDH9DEEFGS7ZKBD&bestFormat=true&previewDoh=1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-700 underline underline-offset-2"
+          >
+            Into the Silent Land by Martin Laird
+          </a>
+        </p>
+        <p>
+          <b>
+            Article on a <i>Rule of Life</i>:{" "}
+          </b>
+          <a
+            href="https://www.chuckdegroat.net/chuck-degroat-blog/longings-over-legalism-in-your-new-years-aspirations"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-700 underline underline-offset-2"
+          >
+            &quot;Longings Over Legalism in Your New Year&apos;s
+            Aspirations&quot; by Chuck Degroat
           </a>
         </p>
       </div>
